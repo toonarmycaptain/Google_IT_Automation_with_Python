293088802e60a676acc184a9d4cae2a97142ae6b
toonarmycaptain <toonarmycaptain@hotmail.com>
2020-02-11 12:04:09 -0600

    Call check_reboot from main, exit with 1 on error.

--- Introduction_to_Git_and_GitHub/all_checks.py
+++ Introduction_to_Git_and_GitHub/all_checks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import sys
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
@@ -7,6 +8,8 @@ def check_reboot():
 
 
 def main():
-    pass
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
 
 main()
