7941da9a186490fc761eb0402f165c11d5dd2b84
toonarmycaptain <toonarmycaptain@hotmail.com>
2020-02-22 21:11:40 -0600

    Add more checks, factor out calls and messages, run when executed directly. Add check_root_full, check_cpu_constrained, check_no_network checks.

--- Introduction_to_Git_and_GitHub/all_checks.py
+++ Introduction_to_Git_and_GitHub/all_checks.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import os
+import shutil
+import socket
 import sys
+
+import psutil
 
 
 def check_reboot():
@@ -8,12 +12,48 @@ def check_reboot():
     return os.path.exists("/run/reboot-required")
 
 
+def check_disk_full(disk, min_gb, min_percent):
+    du = shutil.disk_usage(disk)
+    # Calaculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
+
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+def check_cpu_constrained():
+    """Returns True if the cpu is having too much usage, False otherwise."""
+    return psutil.cpu_percent(1) > 75
+
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise."""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
+
 def main():
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    print("Everything ok.")
+    checks=[
+        (check_reboot, "Pending reboot."),
+        (check_root_full, "Root partition full."),
+        (check_cpu_constrained, "CPU load too high."),
+        (check_no_network, "No working network."),
+    ]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
+
+    print("Everything is ok.")
     sys.exit(0)
 
-
-main()
+if __name__ == '__main__':
+    main()
